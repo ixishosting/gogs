b8704186a69983ca95e7d0b1713336b7ed073e85
Andrew Ashcroft <andrew.ashcroft@ixis.co.uk>
2016-12-01 13:47:15 +0000

    Adding efs

--- docker/start.sh
+++ docker/start.sh
@@ -38,6 +38,7 @@ create_volume_subfolder() {
     done
 }
 
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-69ec13a0.efs.eu-west-1.amazonaws.com:/ /data
 cleanup
 create_volume_subfolder
 
